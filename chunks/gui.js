8780743e4adb8bf21c899577f58376557bd601b6
scratchmasterj21 <john.limpiada@felice.ed.jp>
2024-03-06 16:31:14 +0900

    [skip ci] Build for a41be5739a4a81225704c07e4405d9b044d88c3e

--- chunks/gui.js
+++ chunks/gui.js
@@ -190,6 +190,7 @@ var handleTelemetryModalOptOut = function handleTelemetryModalOptOut() {
     onTelemetryModalOptOut: handleTelemetryModalOptOut
   }) : /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(WrappedGui, {
     canEditTitle: true,
+    backpackVisible: true,
     backpackHost: backpackHost,
     canSave: false,
     onClickLogo: onClickLogo
