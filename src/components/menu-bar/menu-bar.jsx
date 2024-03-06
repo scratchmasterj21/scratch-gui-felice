5e6fa494ec8d5e44c62c5b3fac6aca3091f5773e
scratchmasterj21 <john.limpiada@felice.ed.jp>
2024-03-06 15:14:22 +0900

    scratch menu-bar

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -85,7 +85,7 @@ import aboutIcon from './icon--about.svg';
 import fileIcon from './icon--file.svg';
 import editIcon from './icon--edit.svg';
 
-import scratchLogo from './scratch-logo.svg';
+import scratchLogo from './scratch-felice-logo.png';
 import ninetiesLogo from './nineties_logo.svg';
 import catLogo from './cat_logo.svg';
 import prehistoricLogo from './prehistoric-logo.svg';
